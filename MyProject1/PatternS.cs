4239b7a209e61c502bf3f81044fec4c049f2572d
Snehabramhankar123 <snehabramhankar23@gmail.com>
2022-08-22 17:12:55 +0530

    Program22August

---
+++ MyProject1/PatternS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyProject1
+{
+    class PatternS
+    {
+        static void Main(string[] args)
+        {
+            for (int r= 1;r<=5;r++ )
+            {
+                for(int c =1;c<=r;c++)
+                {
+                    
+                    {
+                        Console.Write(c);
+                    }
+                   
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+ 
+         for(int r=5;)
+
+
+
